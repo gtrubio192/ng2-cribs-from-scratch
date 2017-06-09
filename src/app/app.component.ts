fda413a7e379b5a339963406b399b67328439561
Gabe Rubio <gtrubio192@gmail.com>
2017-06-09 14:21:45 -0600

    Setting up new real estate app utilizing angular 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+// '@' == decorator, here we have a Component decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'this app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { CribListingComponent } from './crib-listing/crib-listing.component';
+// '@' == decorator, decorators take an object
 @NgModule({
+  // tells our app what components we made
   declarations: [
-    AppComponent
+    AppComponent,
+    CribListingComponent
   ],
+  // tells our app what imports we have from angular
   imports: [
     BrowserModule
   ],
+  // tells app which services we are using
   providers: [],
+  // tells app which is the top level component to hook into 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
